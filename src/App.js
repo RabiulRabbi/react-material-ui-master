8dcd8e334fb5459da408937467721ee4ccf7db38
RabiulRabbi <rabiulislam12360@gmail.com>
2020-09-06 22:30:48 +0600

    done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { createContext, useState } from 'react';
+import './App.css';
+import Home from './Components/Home/Home';
+export const CountContext = createContext();
+
+function App() {
+  const [count,setCount] = useState([]);
+  
+  return (
+    <CountContext.Provider value={[count,setCount]}>
+      <Home></Home>
+    </CountContext.Provider>
+  );
+}
+
+export default App;
